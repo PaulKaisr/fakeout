f4e0727585684e023f4fceb71fb5d19721de36f7
paul <paulkaisr@gmail.com>
2025-12-10 21:14:50 +0100

    feat: update project structure and remove unused assets

--- code/frontend/src/pages/index.vue
+++ code/frontend/src/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-card>
+      <v-card-text>
+        <h1>Hello World</h1>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
