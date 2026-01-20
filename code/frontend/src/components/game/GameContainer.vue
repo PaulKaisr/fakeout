a48a324d77546f022b50cae13e57f4e064c64658
paul <paulkaisr@gmail.com>
2026-01-20 23:07:10 +0100

    fix: smaller icon on home page

--- code/frontend/src/components/game/GameContainer.vue
+++ code/frontend/src/components/game/GameContainer.vue
@@ -23,7 +23,7 @@
           class="size-10 rounded-xl overflow-hidden shadow-lg shadow-primary/20"
         >
           <img
-            src="/android-chrome-192x192.png"
+            src="/favicon-32x32.png"
             alt="Fakeout Logo"
             width="40"
             height="40"
